332e409a49dd61fd3359349835d2592f4ff866b0
erickwill <erickwill@gmail.com>
2016-11-13 07:14:08 -0800

    CM-14.1

--- device.mk
+++ device.mk
@@ -1,20 +1,40 @@
 LOCAL_PATH := device/samsung/treltexx
 
-# Flat device tree
+###########################################################
+### FLAT DEVICE TREE
+###########################################################
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dtb.img:dt.img
 
-# Ramdisk
+###########################################################
+### RAMDISK
+###########################################################
+
 PRODUCT_PACKAGES += \
     init.modem.rc
 
-# Audio
+###########################################################
+### RADIO
+###########################################################
+
+# cpboot-daemon for modem
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/ril/sbin/cbd:system/bin/cbd
+
+###########################################################
+### AUDIO
+###########################################################
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-# Bluetooth
+###########################################################
+### BLUETOOTH
+###########################################################
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 
-# Call the proprietary setup
-$(call inherit-product-if-exists, vendor/samsung/treltexx/treltexx-vendor.mk)
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/treltexx/treltexx-vendor.mk)
